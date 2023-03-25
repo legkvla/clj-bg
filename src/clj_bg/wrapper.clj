adbe55a541dfcc52703bf9e854ee3985c86f1858
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2023-03-25 21:42:15 +0400

    Simplifying wrapper

--- src/clj_bg/wrapper.clj
+++ src/clj_bg/wrapper.clj
@@ -4,42 +4,6 @@
     [metrics.core :refer [default-registry]]
     [metrics.timers :refer [timer] :as tmr]))
 
-(defn wrap-handler [handler-id handler-fn data]
-  (let [op-ctx (tmr/start (timer default-registry (name handler-id)))]
-    (try
-      (handler-fn data)
-      (catch Exception ex
-        (error ex
-          (str "Error processing " handler-id))
-        (send-alert (make-alert ex handler-id data))
-        (throw ex))
-      (finally (tmr/stop op-ctx)))))
-
-(defn wrap-user-job [job-id job-fn user]
-  (let [op-ctx (tmr/start (timer default-registry (name job-id)))]
-    (try
-      (job-fn user)
-      (catch Exception ex
-        (error ex
-          (str "Error processing " job-id " for user: " user))
-        (send-alert (make-alert ex job-id user {}))
-        nil)
-      (finally (tmr/stop op-ctx)))))
-
-(defn wrap-user-task [task-id task-fn user data]
-  (let [op-ctx (tmr/start (timer default-registry (name task-id)))]
-    (try
-      (task-fn user data)
-      (catch Exception ex
-        (error ex
-          (str
-            "Error processing " task-id
-            " for user: " user
-            " with data: " data))
-        (send-alert (make-alert ex task-id user data))
-        nil)
-      (finally (tmr/stop op-ctx)))))
-
 (defn wrap-task [task-id data fn:task]
   (let [op-ctx (tmr/start (timer default-registry (name task-id)))]
     (try
